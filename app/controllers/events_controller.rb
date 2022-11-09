cc59e525e9cf9d9d78995d9517b52f568774af19
jonathandreyer <jonathan.dreyer60@gmail.com>
2022-11-09 17:40:55 +0200

    Lovely work

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,19 +1,36 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
 
+
+    # @question = Question.find(params[:question_id])
+    # @answers = @question.answers
+    # @answer = Answer.all
+    @games = Game.all
+    @event = Event.new
+    @events = Event.all
     if params[:query].present?
-      @events = Event.search_by_game_and_date(params[:query])
-      else
-        @events = Event.all
-      end
+      # @events = Event.search_by_address(params[:query])
+      @events = Event.near(params[:query], 5)
+    end
+
+    if params[:gamefilter].present? && params[:gamefilter][:name]
+      name = params[:gamefilter][:name]
+      @game = Game.find_by(name: name)
+      @events = Event.where(game: @game)
+    end
+
+    if params[:start_date].present? && params[:end_date].present?
+      start_date = params[:start_date].to_datetime
+      end_date = params[:end_date].to_datetime
+      @events = Event.where(date: start_date..end_date)
+    end
 
       @markers = @events.geocoded.map do |event|
         {
           lat: event.latitude,
           lng: event.longitude,
-          info_window: render_to_string(partial: "info_window", locals: { event: event }),
-          image_url: helpers.asset_url("LogoGold.png")
+          # info_window: render_to_string(partial: "info_window", locals: { event: event }),
+          # image_url: helpers.asset_url("LogoGold.png")
         }
       end
   end
@@ -21,7 +38,6 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
-
   def new
     @event = Event.new
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -8,8 +8,8 @@ class Event < ApplicationRecord
   has_one_attached :photo
 
   include PgSearch::Model
-      pg_search_scope :search_by_address_and_date,
-      against: [ :address, :date ],
+      pg_search_scope :search_by_address,
+      against: :address,
       using: {
         tsearch: { prefix: true } # <-- now `superman batm` will return something!
   }
