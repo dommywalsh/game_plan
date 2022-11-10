d02267dffe8473f7e3dff9162e2f4be7d6344894
jonathandreyer <jonathan.dreyer60@gmail.com>
2022-11-10 15:21:52 +0200

    Avatar+profilepic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_09_094245) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_10_125638) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -101,6 +101,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_09_094245) do
     t.string "first_name"
     t.string "second_name"
     t.string "address"
+    t.string "profile_pic"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
