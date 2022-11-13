fc5d5cb4f0a87defa6e8b13e2fff0eef09d8bfc1
jonathan_dreyer <jonathan.dreyer60@gmail.com>
2022-11-13 18:10:55 +0200

    A few changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_11_120029) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_13_150940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_11_120029) do
     t.datetime "updated_at", null: false
     t.string "address"
     t.string "name"
+    t.integer "capacity"
     t.index ["game_id"], name: "index_events_on_game_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
