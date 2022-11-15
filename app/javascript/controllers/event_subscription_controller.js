6e4fea921767467d3f20b7572e49f811f83f2d8c
Dominic Walsh <djcwalsh94@gmail.com>
2022-11-15 10:56:25 +0200

    chatroom is doneee

--- app/javascript/controllers/event_subscription_controller.js
+++ app/javascript/controllers/event_subscription_controller.js
@@ -7,6 +7,10 @@ export default class extends Controller {
   static values = {
     eventId: Number,
   };
+  static values = {
+    eventId: Number,
+    currentUserId: Number
+  };
 
   connect() {
     console.log(
@@ -30,7 +34,27 @@ export default class extends Controller {
   }
 
   #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  };
+
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
   }
 }
