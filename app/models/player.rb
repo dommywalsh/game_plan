390a2fda08c76959371da43712c223ae241943f4
Dominic Walsh <djcwalsh94@gmail.com>
2022-11-16 11:24:54 +0200

    notifications done

--- app/models/player.rb
+++ app/models/player.rb
@@ -8,11 +8,11 @@ class Player < ApplicationRecord
     if count == 0
       ""
     elsif count == 1
-      "0 -1px 1px rgba(0,0,0,0.15);"
+      "0 1px 1px rgba(0,0,0,0.15);"
     elsif count == 2
-      "0 -1px 1px rgba(0,0,0,0.15),0 10px 0 -5px #eee,0 10px 1px -4px rgba(0,0,0,0.15);"
+      "0 1px 1px rgba(0,0,0,0.15),0 10px 0 -5px #e8b548,0 10px 1px -4px rgba(0,0,0,0.15);"
     else
-      "0 -1px 1px rgba(0,0,0,0.15),0 10px 0 -5px #eee,0 10px 1px -4px rgba(0,0,0,0.15),0 20px 0 -10px #eee,0 20px 1px -9px rgba(0,0,0,0.15);"
+      "0 1px 1px rgba(0,0,0,0.15),0 10px 0 -5px #e8b548,0 10px 1px -4px rgba(0,0,0,0.15),0 20px 0 -10px #eee,0 20px 1px -9px rgba(0,0,0,0.15);"
     end
   end
 end
