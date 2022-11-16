e575277be7ae667cfc6a33ef9db225fd552f99e8
Dominic Walsh <djcwalsh94@gmail.com>
2022-11-16 12:35:06 +0200

    dashboard is taking shape slowly

--- app/models/player.rb
+++ app/models/player.rb
@@ -12,7 +12,7 @@ class Player < ApplicationRecord
     elsif count == 2
       "0 1px 1px rgba(0,0,0,0.15),0 10px 0 -5px #e8b548,0 10px 1px -4px rgba(0,0,0,0.15);"
     else
-      "0 1px 1px rgba(0,0,0,0.15),0 10px 0 -5px #e8b548,0 10px 1px -4px rgba(0,0,0,0.15),0 20px 0 -10px #eee,0 20px 1px -9px rgba(0,0,0,0.15);"
+      "0 1px 1px rgba(0,0,0,0.15),0 10px 0 -5px #e8b548,0 10px 1px -4px rgba(0,0,0,0.15),0 20px 0 -10px #e8b548,0 20px 1px -9px rgba(0,0,0,0.15);"
     end
   end
 end
