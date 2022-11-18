e70257dd8916ba13f09afb2ab949f671d703f27e
Dominic Walsh <djcwalsh94@gmail.com>
2022-11-18 09:56:55 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 require "faker"
 require "open-uri"
 
@@ -578,8 +571,8 @@ events.each do |event|
     # Event.Player = Player.all.sample
     # player.user = User.all.sample
     player.save
-    end
   end
+end
 
 puts "#{Player.count} players created"
 
